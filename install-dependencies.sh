a667851381511d323a1afadfa6aa64d8801b029c
matheuscardosom <matheuscardoso@gaudium.com.br>
2026-04-14 15:47:18 -0300

    Commit Inicial

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,9 +6,18 @@ mkdir -p yii-app/WebRoot/protected/runtime && chmod 777 yii-app/WebRoot/protecte
 mkdir -p yii-app/WebRoot/protected/migrations && chmod 777 yii-app/WebRoot/protected/migrations
 
 # Composer e dependências
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
 php composer-setup.php
-php -r "unlink('composer-setup.php');"
+rm composer-setup.php
 cd yii-app/YiiRoot
 php ../../composer.phar install
